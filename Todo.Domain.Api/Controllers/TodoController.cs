8794c7ea713b96ec52dfb73d5df11bd0e958c3bc
Abner Matheus <abnerm80@gmail.com>
2021-09-16 13:23:28 -0300

    first commit

---
+++ Todo.Domain.Api/Controllers/TodoController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todo.Domain.Commands;
+using Todo.Domain.Entities;
+using Todo.Domain.Handlers;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/todos")]
+    public class TodoController : ControllerBase
+    {
+        private readonly string _id_user = "bab64c511fd4904466edfbd2a75dff8a";
+
+        #region GET
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll([FromServices] ITodoRepository repository)
+        {
+            var user = _id_user; //User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetAll(user);
+        }
+
+
+        [Route("done")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllDone([FromServices] ITodoRepository repository)
+        {
+            var user = _id_user; //User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetAllDone(user);
+        }
+
+        [Route("undone")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllUndone([FromServices]ITodoRepository repository)
+        {
+            var user = _id_user; //User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetAllUndone(user);
+        }
+
+        [Route("done/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDoneForToday([FromServices] ITodoRepository repository)
+        {
+            var user = _id_user; //User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetByPeriod(user, DateTime.Now.Date, true);
+        }
+
+        [Route("undone/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetInactiveForToday([FromServices] ITodoRepository repository)
+        {
+            var user = _id_user; //User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetByPeriod(user, DateTime.Now.Date, false);
+        }
+
+        [Route("done/tomorrow")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDoneForTomorrow([FromServices] ITodoRepository repository)
+        {
+            var user = _id_user; //User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetByPeriod(user, DateTime.Now.Date.AddDays(1), true);
+        }
+
+        [Route("undone/tomorrow")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetUndoneForTomorrow([FromServices] ITodoRepository repository)
+        {
+            var user = _id_user;//User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetByPeriod(
+                user,
+                DateTime.Now.Date.AddDays(1),
+                false
+            );
+        }
+
+        #endregion GET
+
+        #region POST
+        [HttpPost]        
+        public GenericCommandResult Create([FromBody] CreateTodoCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = _id_user;//User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return (GenericCommandResult)handler.Handle(command);
+        }
+        #endregion POST
+
+        #region PUT
+
+        [HttpPut]
+        public GenericCommandResult Update([FromBody] UpdateTodoCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = _id_user; //User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value; 
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("mark-as-done")]
+        [HttpPut]
+        public GenericCommandResult MarkAsDone([FromBody] MakeTodoAsDoneCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = _id_user; //User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value; 
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("mark-as-undone")]
+        [HttpPut]
+        public GenericCommandResult MarkAsUndone([FromBody] MarkTodoAsUndoneCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = _id_user; //User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value; 
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        #endregion PUT
+
+    }
+
+}
---
+++ Todo.Domain.Api/Startup.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Todo.Domain.Handlers;
+using Todo.Domain.Infra.DataContext;
+using Todo.Domain.Infra.Repositories;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Api
+{
+    public class Startup
+    {   
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<Context>(opt => opt.UseInMemoryDatabase("Database"));
+            //services.AddDbContext<Context>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
+            services.AddTransient<ITodoRepository, TodoRepository>();
+            services.AddTransient<TodoHandler, TodoHandler>();
+
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+            //NECESSÁRIO ESTAR QUANDO HOUVER AUTENTICAÇÃO E AUTORIZAÇÃO VIA TOKEN POR EXEMPLO
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
